65110d175c20970ef74cc31086b7be3231250d03
Anton Agestam <git@antonagestam.se>
2018-03-24 00:21:05 +0100

    new urls format

--- dummy/urls.py
+++ dummy/urls.py
@@ -6,7 +6,7 @@ from text.urls import urlpatterns as django_text_patterns
 from .views import uhn_tiss
 
 urlpatterns = (
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^django_text/', include(django_text_patterns, namespace='django_text')),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^render/$', uhn_tiss, name='render'),
